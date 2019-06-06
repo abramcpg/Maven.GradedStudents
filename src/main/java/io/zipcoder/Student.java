eaf506c5957badebc86e9d08dc4d79d0963db75d
Abram Phillips <abramcpg@hotmail.com>
2019-06-06 17:11:02 -0400

    Part 1.2 complete

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,38 @@
 package io.zipcoder;
+import java.util.ArrayList;
 
 public class Student {
+    String firstName; //Instances
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores){ //Constructor
+
+    }
+    public String getFirstName(){ //Getter
+        return this.firstName;
+    }
+    public void setFirstName(String firstName){ //Setter
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return this.firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(){ //Return number of tests taken by student
+        return null;
+    }
+    public String getExamScores(String firstName, String lastName, Double[] examScores){
+        String scores = "Exam Scores:\n";
+        Integer count = 1;
+        for (Double score : examScores){
+            scores += String.format("\tExam %d -> %.0f\n", count++, score);
+        }
+        return scores;
+    }
+
 }
+
